b4f0ee9b89019aff7d46779b2b9a2b408002a374
289volts <289volts@gmail.com>
2023-02-24 02:55:48 +0100

    [CHORE] Fixed timer card height

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -7,13 +7,13 @@ import bottomBall from "../../public/assets/images/bottom-ball.svg";
 const Countdown = () => {
 	const { setLaunch } = useCountdown();
 
-	const [days, setDays] = useState(0);
-	const [hours, setHours] = useState(0);
-	const [minutes, setMinutes] = useState(0);
-	const [seconds, setSeconds] = useState(0);
+	const [days, setDays] = useState("");
+	const [hours, setHours] = useState(null);
+	const [minutes, setMinutes] = useState(null);
+	const [seconds, setSeconds] = useState(null);
 
 	useEffect(() => {
-		const target = new Date("3/17/2023 23:59:59");
+		const target = new Date("3/24/2023 23:59:59");
 
 		const interval = setInterval(() => {
 			const now = new Date();
@@ -40,37 +40,37 @@ const Countdown = () => {
 	}, []);
 
 	return (
-		<div className="h-screen flex flex-col items-center justify-center bg-body text-white relative overflow-hidden">
-			<div className="">
+		<div className="h-[100dvh] flex flex-col items-center justify-center bg-body text-white relative w-full">
+			<div className="overflow-hidden">
 				<Image src={topBall} alt="" priority className="w-[45%] md:w-[20%] absolute z-[1] top-0 left-0" />
 			</div>
-			<div className="">
+			<div className="overflow-hidden">
 				<Image src={bottomBall} alt="" priority className="w-[45%] md:w-[20%] absolute z-[1] bottom-0 right-0" />
 			</div>
-			<div className="w-[90%] mx-auto text-center">
-				<h1 className="text-[2.5rem] mb-[1.5rem] z-[1] lg:text-[3.5rem]">Launching Soon</h1>
-				<div className="grid grid-cols-4 gap-4 md:gap-6 z-[1] md:w-[50%] mx-auto lg:w-[70%] xl:w-[40%]">
+			<div className="w-[90%] mx-auto text-center relative z-10">
+				<h1 className="text-[2.5rem] mb-[1.5rem] z-[1] lg:text-[3.5rem]">Launching Soon!!</h1>
+				<div className="grid grid-cols-4 gap-4 md:gap-5 lg:gap-6 z-[1] sm:w-[53%] mx-auto lg:w-[55%] xl:w-[40%]">
 					<div className="flex flex-col gap-2 items-center">
-						<div className="backdrop-blur-[10px] bg-white/[0.2] p-4 w-full rounded-[3px] flex items-center justify-center">
-							<span className="text-[2rem] lg:text-[4rem]">{days}</span>
+						<div className="backdrop-blur-[10px] bg-white/[0.2] p-4 w-full rounded-[3px] flex items-center justify-center h-[90px] lg:h-[120px]">
+							<span className="text-[2rem] lg:text-[3rem]">{days}</span>
 						</div>
 						<span className="uppercase text-sm md:text-base lg:text-[1.5rem]">Days</span>
 					</div>
 					<div className="flex flex-col gap-2 items-center">
-						<div className="backdrop-blur-[10px] bg-white/[0.2] p-4 w-full rounded-[3px] flex items-center justify-center">
-							<span className="text-[2rem] lg:text-[4rem]">{hours}</span>
+						<div className="backdrop-blur-[10px] bg-white/[0.2] p-4 w-full rounded-[3px] flex items-center justify-center h-[90px] lg:h-[120px]">
+							<span className="text-[2rem] lg:text-[3rem]">{hours}</span>
 						</div>
 						<span className="uppercase text-sm md:text-base lg:text-[1.5rem]">Hours</span>
 					</div>
 					<div className="flex flex-col gap-2 items-center">
-						<div className="backdrop-blur-[10px] bg-white/[0.2] p-4 w-full rounded-[3px] flex items-center justify-center">
-							<span className="text-[2rem] lg:text-[4rem]">{minutes}</span>
+						<div className="backdrop-blur-[10px] bg-white/[0.2] p-4 w-full rounded-[3px] flex items-center justify-center h-[90px] lg:h-[120px]">
+							<span className="text-[2rem] lg:text-[3rem]">{minutes}</span>
 						</div>
 						<span className="uppercase text-sm md:text-base lg:text-[1.5rem]">Minutes</span>
 					</div>
 					<div className="flex flex-col gap-2 items-center">
-						<div className="backdrop-blur-[10px] bg-white/[0.2] p-4 w-full rounded-[3px] flex items-center justify-center">
-							<span className="text-[2rem] lg:text-[4rem]">{seconds}</span>
+						<div className="backdrop-blur-[10px] bg-white/[0.2] p-4 w-full rounded-[3px] flex items-center justify-center h-[90px] lg:h-[120px]">
+							<span className="text-[2rem] lg:text-[3rem]">{seconds}</span>
 						</div>
 						<span className="uppercase text-sm md:text-base lg:text-[1.5rem]">Seconds</span>
 					</div>
