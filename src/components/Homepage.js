aabf37fe466b53994ae9bd6541e53c55e1e3d13e
289volts <289volts@gmail.com>
2023-03-01 00:50:44 +0100

    [FIX] .

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,6 +3,7 @@ import React from "react";
 import MyLink from "./MyLink";
 import heroImgMobile from "../../public/assets/images/hero-img-mobile.svg";
 import heroImgMobiles from "../../public/assets/images/hero-img-mobile.png";
+import woman from "../../public/assets/images/woman.jpg";
 
 const Homepage = () => {
 	return (
@@ -10,7 +11,7 @@ const Homepage = () => {
 			<section className="">
 				<div className="w-[90%] mx-auto">
 					<div className="">
-						<Image src={heroImgMobiles} alt="" className="w-full" />
+						<Image src={woman} alt="" className="w-[300px]" />
 						<div className="text-center space-y-2 mt-6">
 							<h1 className="font-bold text-lg small:text-[1.25rem] leading-[1.26] normal:text-[1.7rem] md:text-[3rem]">
 								Elevate Your Business with Custom Web Design & Development Solutions
