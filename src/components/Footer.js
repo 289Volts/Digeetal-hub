50e9701ab97d9c1a962894aa30a2168b5391576a
289volts <289volts@gmail.com>
2023-02-28 22:40:19 +0100

    [FEAT] Mobile footer done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,18 +14,18 @@ const Footer = ({ className }) => {
 	const currentYear = new Date().getFullYear();
 	return (
 		<footer className={`${className} bg-primary500 w-full text-whiteText pt-[2.5rem] pb-4`}>
-			<div className="w-[90%] mx-auto">
+			<div className="w-[90%] mx-auto sm:grid sm:grid-cols-2 sm:gap-4 lg:grid-cols-3">
 				<div className="space-y-6">
 					<Image src={footerLogo} className="" alt="footer logo" />
-					<p className="xs:text-xs small:text-sm xs:w-[95%] small:w-[65%] font-bold">
+					<p className="xs:text-xs small:text-sm xs:w-[95%] small:w-[65%] font-bold sm:w-[75%] sm:text-base">
 						We're a team of young, professional and creative experts. Our goal is to deliver top-quality work on time
 						and within budget, while providing exceptional customer service.
 					</p>
 				</div>
-				<div className="mt-6 mb-4">
-					<h2 className="text-xl mb-4 font-bold">Services</h2>
+				<div className="mt-6 mb-4 sm:mt-0">
+					<h2 className="text-xl mb-4 font-bold sm:text-2xl">Services</h2>
 					<nav className="text-black50">
-						<ul className="flex flex-col gap-3 xs:text-xs small:text-sm font-bold">
+						<ul className="flex flex-col gap-3 xs:text-xs small:text-sm font-bold sm:text-base">
 							<li className="">
 								<Link href="" className="">
 									Web & Mobile App Development
@@ -59,10 +59,10 @@ const Footer = ({ className }) => {
 						</ul>
 					</nav>
 				</div>
-				<div className="mt-6 mb-4">
-					<h2 className="text-xl mb-4 font-bold">Quick Links</h2>
+				<div className="mt-6 mb-4 sm:mt-0">
+					<h2 className="text-xs small:text-xl mb-4 font-bold sm:text-2xl">Quick Links</h2>
 					<nav className="text-black50">
-						<ul className="flex flex-col gap-3 xs:text-xs small:text-sm font-bold">
+						<ul className="flex flex-col gap-3 xs:text-xs small:text-sm font-bold sm:text-base">
 							<li className="">
 								<Link href="" className="">
 									Web & Mobile App Development
@@ -95,41 +95,43 @@ const Footer = ({ className }) => {
 							</li>
 						</ul>
 					</nav>
-				</div>
-				<div className="mb-4">
-					<h2 className="text-xl mb-4 font-bold">Contact</h2>
-					<div className="text-black50 xs:text-xs small:text-sm space-y-3">
-						<Link href="" className="flex items-center gap-3 font-bold">
-							<Location className="w-[19px] md:w-[24px] aspect-square" viewBox="0 0 24 24" />
-							<span className="">Lagos, Nigeria</span>
-						</Link>
-						<Link href="" className="flex items-center gap-3 font-bold">
-							<Email className="w-[19px] md:w-[24px] aspect-square" viewBox="0 0 24 24" />
-							<span className="">info@digeetalhub.com</span>
-						</Link>
-						<Link href="" className="flex items-center gap-3 font-bold">
-							<Phone className="w-[19px] md:w-[24px] aspect-square" viewBox="0 0 24 24" />
-							<span className="">+2349035937893</span>
-						</Link>
-					</div>
 				</div>
 				<div className="">
-					<h2 className="small:text-sm xs:text-xs mb-2 font-medium">Follow us on:</h2>
-					<div className="flex gap-2">
-						<Link href="" className="">
-							<Instagram className="w-[28px] md:w-[24px] aspect-square" viewBox="0 0 24 24" />
-						</Link>
-						<Link href="" className="">
-							<Twitter className="w-[28px] md:w-[24px] aspect-square" viewBox="0 0 24 24" />
-						</Link>
-						<Link href="" className="">
-							<LinkedIn className="w-[28px] md:w-[24px] aspect-square" viewBox="0 0 24 24" />
-						</Link>
+					<div className="mb-4">
+						<h2 className="text-xs small:text-xl mb-4 font-bold sm:text-2xl">Contact</h2>
+						<div className="text-black50 xs:text-xs small:text-sm space-y-3 lg:flex sm:text-base">
+							<Link href="" className="flex items-center gap-3 font-bold">
+								<Location className="w-[19px] sm:w-[24px] aspect-square" viewBox="0 0 24 24" />
+								<span className="">Lagos, Nigeria</span>
+							</Link>
+							<Link href="" className="flex items-center gap-3 font-bold">
+								<Email className="w-[19px] sm:w-[24px] aspect-square" viewBox="0 0 24 24" />
+								<span className="">info@digeetalhub.com</span>
+							</Link>
+							<Link href="" className="flex items-center gap-3 font-bold">
+								<Phone className="w-[19px] sm:w-[24px] aspect-square" viewBox="0 0 24 24" />
+								<span className="">+2349035937893</span>
+							</Link>
+						</div>
+					</div>
+					<div className="">
+						<h2 className="small:text-sm xs:text-xs mb-2 font-medium sm:text-base">Follow us on:</h2>
+						<div className="flex gap-3 sm:gap-4">
+							<Link href="" className="">
+								<Instagram className="w-[26px] sm:w-[30px] aspect-square" viewBox="0 0 24 24" />
+							</Link>
+							<Link href="" className="">
+								<Twitter className="w-[26px] sm:w-[30px] aspect-square" viewBox="0 0 24 24" />
+							</Link>
+							<Link href="" className="">
+								<LinkedIn className="w-[26px] sm:w-[30px] aspect-square" viewBox="0 0 24 24" />
+							</Link>
+						</div>
 					</div>
 				</div>
 			</div>
 			<div className="border-t-2 border-t-whiteText mt-4">
-				<p className="text-xs xs:text-xs mt-4 flex justify-center text-black50 items-center text-center">
+				<p className="small:text-sm xs:text-xs mt-4 flex justify-center text-black50 items-center text-center sm:text-base">
 					Copyright &copy; {currentYear} Digeetal Hub. All rights reserved.
 				</p>
 			</div>
